c08ec58baa12ba4754dbd57a127fa78abda8dacd
MMartinsR <mariana.martins.rosa.mmr@gmail.com>
2023-02-07 22:20:23 +0000

    2.classes-modelo

---
+++ src/main/java/projetotcc/model/Tarefa.java
@@ -0,0 +1,93 @@
+package projetotcc.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Tarefa implements Serializable, Base{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String peso;
+	private Date dataEntrega;
+	private String status;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getPeso() {
+		return peso;
+	}
+
+	public void setPeso(String peso) {
+		this.peso = peso;
+	}
+
+	public Date getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(Date dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/projetotcc/model/Usuario.java
@@ -0,0 +1,74 @@
+package projetotcc.model;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable, Base {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String email;
+	private String nomeExibicao;
+	private String senha;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getNomeExibicao() {
+		return nomeExibicao;
+	}
+	
+	public void setNomeExibicao(String nomeExibicao) {
+		this.nomeExibicao = nomeExibicao;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+}
