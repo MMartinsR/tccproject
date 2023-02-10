1ed0c2874612b04d6286954905f9ae0585bc81f6
MMartinsR <mariana.martins.rosa.mmr@gmail.com>
2023-02-10 17:41:50 +0000

    5. Dashboard-inicial
    Esboço inicial da dashboard, dialog de criação de projeto, Implementação inicial do MB, Service, anotações de persistencia na classe Projeto.

--- src/main/java/projetotcc/controller/ProjetoMB.java
+++ src/main/java/projetotcc/controller/ProjetoMB.java
@@ -1,8 +1,88 @@
 package projetotcc.controller;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import projetotcc.model.Projeto;
+import projetotcc.service.ProjetoService;
+
 @Named
-public class ProjetoMB {
+@ViewScoped
+public class ProjetoMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Projeto projeto = new Projeto();
+	
+	@Inject
+	private ProjetoService projetoService;
+		
+	private List<Projeto> projetos = new ArrayList<>();	
+	private Projeto projetoSelecionado;
+	
+	private String dataCriada;
+	
+
+	public void init() {
+		System.out.println("entrou no init!");
+		this.projetos = projetoService.listarTodos();
+		gerarData();
+	}
+	
+	
+	public void abrirNovo() {
+		this.projetoSelecionado = new Projeto();
+	}
+	
+	private void gerarData() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date data = new Date();
+		
+		setDataCriada(sdf.format(data));
+	}
+	
+	public void salvarProjeto() {
+		
+	}
+	
+	
+	public Projeto getProjeto() {
+		return projeto;
+	}
+
+	public void setProjeto(Projeto projeto) {
+		this.projeto = projeto;
+	}
+
+	public Projeto getProjetoSelecionado() {
+		return projetoSelecionado;
+	}
+
+	public void setProjetoSelecionado(Projeto projetoSelecionado) {
+		this.projetoSelecionado = projetoSelecionado;
+	}
+
+	public List<Projeto> getProjetos() {
+		return projetos;
+	}
+
+	public String getDataCriada() {
+		return dataCriada;
+	}
+
+	public void setDataCriada(String dataCriada) {
+		this.dataCriada = dataCriada;
+	}
+	
+	
+	
 
 }
--- src/main/java/projetotcc/model/Projeto.java
+++ src/main/java/projetotcc/model/Projeto.java
@@ -3,15 +3,30 @@ package projetotcc.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "tb_projeto")
 public class Projeto implements Serializable, Base {
 
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false, length = 60)
 	private String nome;
 	private String descricao;
+	@Temporal(TemporalType.DATE)
 	private Date dataCriacao;
+	@Column(nullable = false)
 	private String criador;
 	
 	
